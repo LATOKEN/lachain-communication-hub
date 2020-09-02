2a88af102fd6c51a184ad80a357a473fc8c30695
Ivan Zakharov <ivan.zakharov@latoken.com>
2020-09-02 12:26:38 +0300

    Dirty fast hack to set ipv4 addr lookup

--- utils/ipLookup.go
+++ utils/ipLookup.go
@@ -8,6 +8,7 @@ func IPLookup() string {
 	// Create the default consensus,
 	// using the default configuration and no logger.
 	consensus := externalip.DefaultConsensus(nil, nil)
+	consensus.AddVoter(externalip.NewHTTPSource("https://ipv4.icanhazip.com/"), 6)
 	// Get your IP,
 	// which is never <nil> when err is <nil>.
 	ip, err := consensus.ExternalIP()
