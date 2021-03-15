e34855f1a825869a8cf06c7d706e6081f2882c31
sgladkov <sergey.gladkov@latoken.com>
2021-03-15 18:21:46 +0300

    generator for remote configs and goverment scripts

--- testconfiggen.go
+++ testconfiggen.go
@@ -9,6 +9,7 @@ import (
 	p2p_crypto "github.com/libp2p/go-libp2p-core/crypto"
 	p2p_peer "github.com/libp2p/go-libp2p-core/peer"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -18,19 +19,14 @@ type Config struct {
 	Bootstraps string `json: "bootstraps"`
 }
 
-
-func main() {
-	var peerNumber uint
-	flag.UintVar(&peerNumber, "peerNumber", 4, "Specify number of peers in test,  default value is 4", )
-	flag.Parse()
-
+func generateLocalConfig(peerNumber uint, port uint) {
 	privateKeys := make([]p2p_crypto.PrivKey, peerNumber, peerNumber)
 	for i := uint(0); i < peerNumber; i++ {
 		privateKeys[i], _, _ = p2p_crypto.GenerateECDSAKeyPair(rand.Reader)
 	}
 
 	peerList := ""
-	nodePort := uint32(7070)
+	nodePort := uint32(port)
 	for i := uint(0); i < peerNumber; i++ {
 		id, _ := p2p_peer.IDFromPrivateKey(privateKeys[i])
 		bootstrapAddress := fmt.Sprintf("%s@127.0.0.1:%d", p2p_peer.Encode(id), nodePort)
@@ -41,7 +37,7 @@ func main() {
 		nodePort++
 	}
 
-	nodePort = 7070
+	nodePort = uint32(port)
 	for i := uint(0); i < peerNumber; i++ {
 		prvBytes, err := p2p_crypto.MarshalPrivateKey(privateKeys[i])
 		if err != nil {
@@ -83,5 +79,108 @@ func main() {
 	}
 	f.Sync()
 	f.Close()
+}
 
+func generateRemoteConfig(ips string, port uint, sshkeypath string) {
+	ipList := strings.Split(ips, ",")
+	peerNumber := len(ipList)
+	privateKeys := make([]p2p_crypto.PrivKey, peerNumber, peerNumber)
+	for i := 0; i < peerNumber; i++ {
+		privateKeys[i], _, _ = p2p_crypto.GenerateECDSAKeyPair(rand.Reader)
+	}
+
+	peerList := ""
+	for i := 0; i < peerNumber; i++ {
+		id, _ := p2p_peer.IDFromPrivateKey(privateKeys[i])
+		bootstrapAddress := fmt.Sprintf("%s@%s:%d", p2p_peer.Encode(id), ipList[i], port)
+		peerList += bootstrapAddress
+		if i != peerNumber -1 {
+			peerList += ","
+		}
+	}
+
+	for i := 0; i < peerNumber; i++ {
+		prvBytes, err := p2p_crypto.MarshalPrivateKey(privateKeys[i])
+		if err != nil {
+			panic(err)
+		}
+
+		data := Config{
+			PrivateKey: hex.EncodeToString(prvBytes),
+			Idx:        uint32(i),
+			Port:       uint32(port),
+			Bootstraps: peerList,
+		}
+
+		databin,  err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+
+		filename := fmt.Sprintf("testhubconfig%d.json", i)
+		f, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		f.Write(databin)
+		f.Sync()
+		f.Close()
+	}
+
+	f, err := os.Create("./deploy.sh")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < peerNumber; i++ {
+		line := fmt.Sprintf("scp -i %s ./testhub root@%s:testhub\n", sshkeypath, ipList[i])
+		f.WriteString(line)
+		line = fmt.Sprintf("scp -i %s ./testhubconfig%d.json root@%s:testhubconfig.json\n", sshkeypath, i,
+			ipList[i])
+		f.WriteString(line)
+	}
+	f.Sync()
+	f.Close()
+
+	f, err = os.Create("./starthubs.sh")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < peerNumber; i++ {
+		line := fmt.Sprintf("ssh -i %s root@%s './testhub -config ./testhubconfig.json' &\n", sshkeypath, ipList[i])
+		f.WriteString(line)
+	}
+	f.Sync()
+	f.Close()
+
+	f, err = os.Create("./remove.sh")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < peerNumber; i++ {
+		line := fmt.Sprintf("ssh -i %s root@%s 'rm -rf ./testhub'\n", sshkeypath, ipList[i])
+		f.WriteString(line)
+		line = fmt.Sprintf("ssh -i %s root@%s 'rm -rf ./testhubconfig.json'\n", sshkeypath, ipList[i])
+		f.WriteString(line)
+	}
+	f.Sync()
+	f.Close()
+}
+
+func main() {
+	var peerNumber uint
+	var port uint
+	var ips string
+	var sshkeypath string
+	flag.UintVar(&peerNumber, "peerNumber", 4, "Specify number of peers in test, should match number of peers in ips for remote tests, default value is 4", )
+	flag.UintVar(&port, "port", 7070, "Base port value for local tests and port for remote hosts, default value is 7070", )
+	flag.StringVar(&ips, "ips", "", "Comma-separated list of hosts for remote tests,  empty for local tests, default value is empty", )
+	flag.StringVar(&sshkeypath, "key", "", "Path to key file to use in ssh for remote tests, empty for local tests, default value is empty", )
+	flag.Parse()
+
+	if len(ips) > 0 {
+		generateRemoteConfig(ips, port, sshkeypath)
+	} else {
+		generateLocalConfig(peerNumber, port)
+	}
 }
