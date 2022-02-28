102694a0cf767b3b759badf38b3996334768737b
kunalgoyal9 <kunalgoyal.goyal9@gmai.com>
2022-02-28 15:29:24 +0530

    added killing background processes

--- testconfiggen.go
+++ testconfiggen.go
@@ -6,10 +6,11 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	p2p_crypto "github.com/libp2p/go-libp2p-core/crypto"
-	p2p_peer "github.com/libp2p/go-libp2p-core/peer"
 	"os"
 	"strings"
+
+	p2p_crypto "github.com/libp2p/go-libp2p-core/crypto"
+	p2p_peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
 type Config struct {
@@ -20,7 +21,7 @@ type Config struct {
 	Repeats    uint32 `json: "repeatCount"`
 }
 
-func generateLocalConfig(peerNumber uint, port uint,  repeat uint) {
+func generateLocalConfig(peerNumber uint, port uint, repeat uint) {
 	privateKeys := make([]p2p_crypto.PrivKey, peerNumber, peerNumber)
 	for i := uint(0); i < peerNumber; i++ {
 		privateKeys[i], _, _ = p2p_crypto.GenerateECDSAKeyPair(rand.Reader)
@@ -32,7 +33,7 @@ func generateLocalConfig(peerNumber uint, port uint,  repeat uint) {
 		id, _ := p2p_peer.IDFromPrivateKey(privateKeys[i])
 		bootstrapAddress := fmt.Sprintf("%s@127.0.0.1:%d", p2p_peer.Encode(id), nodePort)
 		peerList += bootstrapAddress
-		if i != peerNumber -1 {
+		if i != peerNumber-1 {
 			peerList += ","
 		}
 		nodePort++
@@ -50,10 +51,10 @@ func generateLocalConfig(peerNumber uint, port uint,  repeat uint) {
 			Idx:        uint32(i),
 			Port:       nodePort,
 			Bootstraps: peerList,
-			Repeats: 	uint32(repeat),
+			Repeats:    uint32(repeat),
 		}
 
-		databin,  err := json.Marshal(data)
+		databin, err := json.Marshal(data)
 		if err != nil {
 			panic(err)
 		}
@@ -83,7 +84,7 @@ func generateLocalConfig(peerNumber uint, port uint,  repeat uint) {
 	f.Close()
 }
 
-func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint) {
+func generateRemoteConfig(ips string, port uint, sshkeypath string, repeat uint) {
 	ipList := strings.Split(ips, ",")
 	peerNumber := len(ipList)
 	privateKeys := make([]p2p_crypto.PrivKey, peerNumber, peerNumber)
@@ -96,7 +97,7 @@ func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint
 		id, _ := p2p_peer.IDFromPrivateKey(privateKeys[i])
 		bootstrapAddress := fmt.Sprintf("%s@%s:%d", p2p_peer.Encode(id), ipList[i], port)
 		peerList += bootstrapAddress
-		if i != peerNumber -1 {
+		if i != peerNumber-1 {
 			peerList += ","
 		}
 	}
@@ -112,10 +113,10 @@ func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint
 			Idx:        uint32(i),
 			Port:       uint32(port),
 			Bootstraps: peerList,
-			Repeats: 	uint32(repeat),
+			Repeats:    uint32(repeat),
 		}
 
-		databin,  err := json.Marshal(data)
+		databin, err := json.Marshal(data)
 		if err != nil {
 			panic(err)
 		}
@@ -136,9 +137,9 @@ func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint
 		panic(err)
 	}
 	for i := 0; i < peerNumber; i++ {
-		line := fmt.Sprintf("scp -i %s ./testhub root@%s:testhub\n", sshkeypath, ipList[i])
+		line := fmt.Sprintf("scp -o \"StrictHostKeyChecking no\" -i %s ./testhub root@%s:testhub\n", sshkeypath, ipList[i])
 		f.WriteString(line)
-		line = fmt.Sprintf("scp -i %s ./testhubconfig%d.json root@%s:testhubconfig.json\n", sshkeypath, i,
+		line = fmt.Sprintf("scp -o \"StrictHostKeyChecking no\"  -i %s ./testhubconfig%d.json root@%s:testhubconfig.json\n", sshkeypath, i,
 			ipList[i])
 		f.WriteString(line)
 	}
@@ -150,7 +151,7 @@ func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint
 		panic(err)
 	}
 	for i := 0; i < peerNumber; i++ {
-		line := fmt.Sprintf("ssh -i %s root@%s './testhub -config ./testhubconfig.json' &\n", sshkeypath, ipList[i])
+		line := fmt.Sprintf("ssh -o \"StrictHostKeyChecking no\" -i %s root@%s './testhub -config ./testhubconfig.json' &\n", sshkeypath, ipList[i])
 		f.WriteString(line)
 	}
 	f.Sync()
@@ -161,9 +162,20 @@ func generateRemoteConfig(ips string, port uint, sshkeypath string,  repeat uint
 		panic(err)
 	}
 	for i := 0; i < peerNumber; i++ {
-		line := fmt.Sprintf("ssh -i %s root@%s 'rm -rf ./testhub'\n", sshkeypath, ipList[i])
+		line := fmt.Sprintf("ssh -o \"StrictHostKeyChecking no\" -i %s root@%s 'rm -rf ./testhub' &\n", sshkeypath, ipList[i])
 		f.WriteString(line)
-		line = fmt.Sprintf("ssh -i %s root@%s 'rm -rf ./testhubconfig.json'\n", sshkeypath, ipList[i])
+		line = fmt.Sprintf("ssh -o \"StrictHostKeyChecking no\" -i %s root@%s 'rm -rf ./testhubconfig.json' &\n", sshkeypath, ipList[i])
+		f.WriteString(line)
+	}
+	f.Sync()
+	f.Close()
+
+	f, err = os.Create("./kill.sh")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < peerNumber; i++ {
+		line := fmt.Sprintf("ssh -o \"StrictHostKeyChecking no\" -i %s root@%s 'killall -9 testhub' &\n", sshkeypath, ipList[i])
 		f.WriteString(line)
 	}
 	f.Sync()
@@ -176,16 +188,16 @@ func main() {
 	var ips string
 	var sshkeypath string
 	var repeat uint
-	flag.UintVar(&peerNumber, "peerNumber", 4, "Specify number of peers in test, should match number of peers in ips for remote tests, default value is 4", )
-	flag.UintVar(&port, "port", 7070, "Base port value for local tests and port for remote hosts, default value is 7070", )
-	flag.StringVar(&ips, "ips", "", "Comma-separated list of hosts for remote tests,  empty for local tests, default value is empty", )
-	flag.StringVar(&sshkeypath, "key", "", "Path to key file to use in ssh for remote tests, empty for local tests, default value is empty", )
-	flag.UintVar(&repeat, "repeat", 100, "Repeat count for RBC emulation, default value is 100", )
+	flag.UintVar(&peerNumber, "peerNumber", 4, "Specify number of peers in test, should match number of peers in ips for remote tests, default value is 4")
+	flag.UintVar(&port, "port", 7070, "Base port value for local tests and port for remote hosts, default value is 7070")
+	flag.StringVar(&ips, "ips", "", "Comma-separated list of hosts for remote tests,  empty for local tests, default value is empty")
+	flag.StringVar(&sshkeypath, "key", "", "Path to key file to use in ssh for remote tests, empty for local tests, default value is empty")
+	flag.UintVar(&repeat, "repeat", 100, "Repeat count for RBC emulation, default value is 100")
 	flag.Parse()
 
 	if len(ips) > 0 {
 		generateRemoteConfig(ips, port, sshkeypath, repeat)
 	} else {
-		generateLocalConfig(peerNumber, port,  repeat)
+		generateLocalConfig(peerNumber, port, repeat)
 	}
 }
